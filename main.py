26dcde79c001ff6336641781313dff4df3e3196f
haiminh <haiminhht2k4@gmail.com>
2025-07-24 16:04:20 +0700

    new test

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import RPi.GPIO as GPIO
 import time
 
-LED_PIN = 12     # GPIO 17 nối với LED (chân số 11)
-SOUND_PIN = 27     # GPIO 27 nhận tín hiệu từ cảm biến âm thanh (DO)
-
+# Sử dụng số chân theo kiểu BCM
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(LED_PIN, GPIO.OUT)
-GPIO.setup(SOUND_PIN, GPIO.IN)
 
-led_state = False
+# Cài đặt chân GPIO 17 làm output
+LED_PIN = 12
+GPIO.setup(LED_PIN, GPIO.OUT)
 
 try:
-    while True:
-        if GPIO.input(SOUND_PIN) == 0:  # 0 = phát hiện âm thanh (vỗ tay, gõ bàn)
-            print("Phát hiện âm thanh!")
-            led_state = not led_state
-            GPIO.output(LED_PIN, led_state)
-            time.sleep(0.3)  # Chống nhiễu
-        time.sleep(0.05)
-except KeyboardInterrupt:
-    pass
+    # Bật đèn
+    GPIO.output(LED_PIN, GPIO.HIGH)
+    print("Đèn đã bật.")
+    
+    time.sleep(5)  
+    
+    # Tắt đèn
+    GPIO.output(LED_PIN, GPIO.LOW)
+    print("Đèn đã tắt.")
+    
 finally:
+    # Reset các thiết lập GPIO
     GPIO.cleanup()
---
+++ test.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install python3-pip portaudio19-dev
+pip3 install SpeechRecognition pyaudio
+
+
+
+
+
